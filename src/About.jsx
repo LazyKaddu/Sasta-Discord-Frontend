72cb05d62fcf092b0870e19444d2ee76bd02aa0f
Nikhil <nikhilbhatt220705@gmail.com>
2024-07-07 16:51:09 +0530

    I forgot what i Changed

--- src/About.jsx
+++ src/About.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 import MainBody from './components/MainBody';
 import AboutSection from './components/AboutSection';
 
-const ChatSection = ()=>{
-    return(
-        <>
-        <Title title={'GroupChat-1'}/>
-        <MessagesSec/>
-        <Chatbox/>
-        </>
-    )
-}
 
 const Chat = () => {
   return (
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Chat from "./Chat";
