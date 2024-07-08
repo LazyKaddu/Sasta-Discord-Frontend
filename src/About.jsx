72affd025555fa6ff6b89f2836c984e08a78189f
Aashish Negi <aashishnegi0000@gmail.com>
2024-07-08 10:09:19 +0530

    fixed about

--- src/About.jsx
+++ src/About.jsx
@@ -3,19 +3,9 @@ import MainBody from './components/MainBody';
 import AboutSection from './components/AboutSection';
 
 
-const ChatSection = ()=>{
-    return(
-        <>
-        <Title title={'GroupChat-1'}/>
-        <MessagesSec/>
-        <Chatbox/>
-        </>
-    )
-}
-
 const Chat = () => {
   return (
-    <MainBody data={<AboutSection/>} open={false}/>
+    <MainBody data={<AboutSection/>} open={true}/>
   )
 }
 
