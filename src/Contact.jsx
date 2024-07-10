c9d73332b817bfe47bbe3b585d24d20df016060d
Aashish Negi <aashishnegi0000@gmail.com>
2024-07-10 16:14:59 +0530

    fixed bugs in contactus

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import MainBody from "./components/MainBody";
+import { useNavigate } from "react-router-dom";
 
 const ContactSection = () => {
+
+  const Navigate = useNavigate();
+  const [FullName, setFullName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Message, setMessage] = useState('');
+
+
+  const setname = (e)=>{
+    setFullName(e.target.value);
+  }
+  const setemail = (e)=>{
+    setEmail(e.target.value);
+  }
+  const setmessage = (e)=>{
+    setMessage(e.target.value);
+  }
+
+
+  const submitForm = (e)=>{
+    e.preventDefault();
+    axios.post('./contacted',{
+      props:{
+        FullName,
+        Email,
+        Message,
+      }
+    }).then(
+      Navigate('/contact-us')
+    ).catch((e)=>{
+      console.log(e)
+    }
+    )
+  }
   return (
     <>
       <div className='size-full bg-[#5865F2] bg-[url("https://imgs.search.brave.com/zqC78uQ44KD1yV4FGJNlXrVIwe8ddJ-2_L5FYHZo4sQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/d2FsbHBhcGVyc2Fm/YXJpLmNvbS8yNi82/OC9DSzdKUVouanBn")] bg-cover grid place-items-center'>
@@ -64,41 +98,41 @@ const ContactSection = () => {
             <div className="flex flex-col h-fit py-2 w-[45%] bg-white rounded">
               <form
                 className="flex flex-col gap-2 h-full w-[90%] text-zinc-700 px-5 py-2 mb-3"
-                method="post"
+                onSubmit={submitForm}
               >
                 <h3 className="text-4xl font-bold my-1">Send Message</h3>
                 <div id="contact-input" className="w-full relative mt-2">
                   <input
                     className="w-full py-1 text-lg my-2 border-b-2 border-zinc-700 outline-none"
-                    type="text"
+                    type="text" value={FullName} onChange={setname}
                   />
-                  <span
+                  {!FullName && <span
                     id="contact-span"
                     className="absolute left-0 py-1 text-lg my-2 text-zinc-600 pointer-events-none"
                   >
                     Full Name
-                  </span>
+                  </span>}
                 </div>
                 <div id="contact-input" className="w-full relative mt-2">
                   <input
                     className="w-full py-1 text-lg my-2 border-b-2 border-zinc-700 outline-none"
-                    type="email"
+                    type="email" value={Email} onChange={setemail}
                   />
-                  <span
+                  {!Email && <span
                     id="contact-span"
                     className="absolute left-0 py-1 text-lg my-2 text-zinc-600 pointer-events-none"
                   >
                     Email
-                  </span>
+                  </span>}
                 </div>
                 <div id="contact-input" className="w-full relative mt-2">
-                  <textarea className="resize-none w-full py-1 text-lg my-2 border-b-2 border-zinc-700 outline-none"></textarea>
-                  <span
+                  <textarea className="resize-none w-full py-1 text-lg my-2 border-b-2 border-zinc-700 outline-none" value={Message} onChange={setmessage}></textarea>
+                  {!Message && <span
                     id="contact-span"
                     className="absolute left-0 py-1 text-lg my-2 text-zinc-600 pointer-events-none"
                   >
                     Type your Message...
-                  </span>
+                  </span>}
                 </div>
                 <div>
                   <button
