3a80c4c65aed5c7b17eabe9d87ecfdce5f8aa7ed
Aashish Negi <aashishnegi0000@gmail.com>
2024-07-13 12:18:00 +0530

    ab mai bhool gaya kya kar atha change

--- src/components/ServerSection.jsx
+++ src/components/ServerSection.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ServersList from "./ServersList";
 import axios from "axios";
 import Cookies from "js-cookie";
-import {HiOutlineServer, HiOutlinePlus} from "react-icons/hi";
+import { HiOutlineServer, HiOutlinePlus } from "react-icons/hi";
+
+axios.defaults.withCredentials = true;
 
 const ServerSection = ({ allServers }) => {
   const userId = Cookies.get("userId");
   const [Error, setError] = useState(null);
-  // axios
-  //   .get("./api/channels", {
-  //     params: {
-  //       userId: userId,
-  //     },
-  //   })
-  //   .then((response) => {
-  //     //setChats(response.data)
-  //   })
-  //   .catch((error) => {
-  //     setError(error);
-  //   });
+
+  useEffect(() => {
+  
+    axios
+      .get("./api/channels", {
+        withCredentials:true,
+        params: {
+          userId: userId,
+        },
+        headers:{
+          Authorization:`Bearer ${Cookies.get("token")}`},
+      })
+      .then((response) => {
+        //setChats(response.data)
+      })
+      .catch((error) => {
+        setError(error);
+      });
+
+  }, [])
+  
+
 
   return (
     <div className="h-full flex flex-col items-center gap-1 p">
